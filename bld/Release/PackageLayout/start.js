66d4655609f6452d191e68bb3fba7994c5c50924
Nitesh Sahni <nsniteshsahni.007@gmail.com>
2016-07-10 14:57:16 +0530

    Project created

---
+++ bld/Release/PackageLayout/start.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize your application here.
+            } else {
+                // TODO: This application was suspended and then terminated.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+    };
+
+    app.start();
+
+    window["cr_windows10"] = true;
+
+    // Create new runtime using the c2canvas
+    cr_createRuntime("c2canvas");
+})();
+
+
+// Size the canvas to fill the browser viewport.
+jQuery(window).resize(function() {
+	cr_sizeCanvas(jQuery(window).width(), jQuery(window).height());
+});
+
+// Pause and resume on page becoming visible/invisible
+function onVisibilityChanged() {
+	if (document.hidden || document.mozHidden || document.webkitHidden || document.msHidden)
+		cr_setSuspended(true);
+	else
+		cr_setSuspended(false);
+};
+
+document.addEventListener("visibilitychange", onVisibilityChanged, false);
+document.addEventListener("mozvisibilitychange", onVisibilityChanged, false);
+document.addEventListener("webkitvisibilitychange", onVisibilityChanged, false);
+document.addEventListener("msvisibilitychange", onVisibilityChanged, false);
